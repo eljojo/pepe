d7cb69b2a627a2968c0b45060fe3aaedce9ccf76
José Tomás Albornoz <jojo@eljojo.net>
2016-05-29 14:40:44 -0400

    remove db credentials

--- config/dev.exs
+++ config/dev.exs
@@ -36,8 +36,6 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :pepe, Pepe.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
   database: "pepe_dev",
   hostname: "localhost",
   pool_size: 10
