673447d42a3b973aa777c8a62cb6b5dee5cfbaa3
José Tomás Albornoz <jojo@eljojo.net>
2016-06-05 02:22:12 -0400

    improve image size

--- web/views/page_view.ex
+++ web/views/page_view.ex
@@ -5,10 +5,11 @@ defmodule Pepe.PageView do
     tweet_counts |> Enum.map(&(&1.count)) |> Enum.sum
   end
 
-  def tweet_count_image(tweet_count, sum) do
+  def tweet_count_image(tweet_count, tweet_counts) do
+    sum = sum_counts(tweet_counts)
     percentage = tweet_count.count / sum
     twitter_user = tweet_count.twitter_user
-    display_size = 3000 * percentage
+    display_size = length(tweet_counts) * 20 * percentage
     options = [
       src: twitter_user.avatar,
       title: "@#{twitter_user.screen_name}: #{Float.round(percentage * 100, 2)}%",
