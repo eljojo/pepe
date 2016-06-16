331932b9b889ec6fdcea8675e0b39e2f97b9449c
José Tomás Albornoz <jojo@eljojo.net>
2016-06-15 23:43:33 -0400

    improve performance slightly of page_view

--- web/views/page_view.ex
+++ web/views/page_view.ex
@@ -5,11 +5,10 @@ defmodule Pepe.PageView do
     tweet_counts |> Enum.map(&(&1.count)) |> Enum.sum
   end
 
-  def tweet_count_image(tweet_count, tweet_counts) do
-    sum = sum_counts(tweet_counts)
+  def tweet_count_image(tweet_count, length_tweet_counts, sum) do
     percentage = tweet_count.count / sum
     twitter_user = tweet_count.twitter_user
-    display_size = length(tweet_counts) * 20 * percentage
+    display_size = length_tweet_counts * 20 * percentage
     options = [
       src: twitter_user.avatar,
       title: "@#{twitter_user.screen_name}: #{Float.round(percentage * 100, 2)}%",
