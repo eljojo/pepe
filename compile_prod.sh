7abf4bd0e313964cd07ec2a0f5cbdf0ac12103e4
José Albornoz <jojo@eljojo.net>
2016-10-08 22:17:58 -0400

    remove prod scripts

--- compile_prod.sh
+++
@@ -1,6 +0,0 @@
-git pull origin master
-mix deps.get --only prod
-MIX_ENV=prod mix compile
-./node_modules/.bin/brunch build --production
-MIX_ENV=prod mix phoenix.digest
-MIX_ENV=prod mix ecto.migrate
--- start_prod.sh
+++
@@ -1,1 +0,0 @@
-PORT=3001 MIX_ENV=prod iex -S mix phoenix.server
