d717943bb8ca9626f23296e3be7fe05adca8442f
José Albornoz <jojo@eljojo.net>
2016-10-08 22:07:58 -0400

    heroku!

---
+++ config/prod.secret.exs
@@ -0,0 +1,19 @@
+use Mix.Config
+
+
+
+use Mix.Config
+
+config :pepe, Pepe.Endpoint,
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
+
+config :extwitter, :oauth, [
+  consumer_key: System.get_env("TWITTER_CONSUMER_KEY"),
+  consumer_secret: System.get_env("TWITTER_CONSUMER_SECRET")
+]
+
+config :pepe, Pepe.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  size: 20
+
